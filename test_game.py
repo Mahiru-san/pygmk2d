8eeee2d8a6b732b7d47d45580883e77e3716c314
SteveNotJosh183 <minhmoc2709@gmail.com>
2025-10-11 10:54:36 +0700

    Refactor ball spawning logic and collision handling; adjust random ball generation parameters and add controls for dynamic ball size

--- test_game.py
+++ test_game.py
@@ -12,6 +12,8 @@ class QuadTreeRenderer:
         for node in obj.iterate_nodes():
             # if len(node.container) == 0:
             #    continue
+            # if node.depth != 3:
+            #    continue
             self._draw_node(screen, node)
 
     def _draw_node(self, screen: pygame.surface.Surface, node: QuadTreeNode) -> None:
@@ -51,7 +53,7 @@ def strict_collision_update(container: list[int], reference_list: list[Ball]) ->
                     reference_list[container[i]], reference_list[container[j]]
                 )
                 exchange_momentum(
-                    reference_list[container[i]], reference_list[container[j]], 0.999
+                    reference_list[container[i]], reference_list[container[j]], 0.001
                 )
 
 
@@ -68,12 +70,12 @@ def generate_random_balls(
     ]
     width, height = int(resolution[0]), int(resolution[1])
     for _ in range(quantity):
-        random_size_and_mass = 5  # random.randint(1, 20)
+        random_size_and_mass = 1  # random.randint(1, 20)
         random_position = (
-            random.uniform(0, width) - random_size_and_mass,
-            random.uniform(0, height) - random_size_and_mass,
+            random.uniform(random_size_and_mass, width - random_size_and_mass),
+            random.uniform(random_size_and_mass, height - random_size_and_mass),
         )
-        max_velocity = 1000
+        max_velocity = 200
         random_velocity = (
             random.uniform(-max_velocity, max_velocity),
             random.uniform(-max_velocity, max_velocity),
@@ -95,9 +97,9 @@ class TestGame(Game):
         self.render_controller.register_renderer("quadtree", QuadTreeRenderer())
 
         self.game_object_manager.add_multi(
-            generate_random_balls(500, self.render_controller.get_resolution())
+            generate_random_balls(1000, self.render_controller.get_resolution())
         )
-        preset_ball_list = [
+        """preset_ball_list = [
             Ball(
                 100,
                 (50, self.render_controller.get_resolution()[1] / 2),
@@ -105,9 +107,29 @@ class TestGame(Game):
                 10,
                 (0, 0),
             ),
-        ]
-        self.game_object_manager.add_multi(preset_ball_list)
+            Ball(
+                100,
+                (40, self.render_controller.get_resolution()[1] / 2),
+                color.WHITE,
+                20,
+                (100, 0),
+            ),
+        ]"""
+        """preset_ball_list = []
+        for i in range(0, 50):
+            preset_ball_list.append(
+                Ball(
+                    1,
+                    ((10 + 20 * i), self.render_controller.get_resolution()[1] / 2),
+                    color.WHITE,
+                    1,
+                    (500 + 20 * i, 0),
+                )
+            )"""
+        # self.game_object_manager.add_multi(preset_ball_list)
         self.set_fps(240)
+        self.spawn_time_marker = 0
+        self.spawn_ball_size = 2
 
     def update(self) -> None:
         self._ball_collision_update()
@@ -146,6 +168,42 @@ class TestGame(Game):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.stop()
+                if event.key == pygame.K_r:
+                    self.game_object_manager.remove_pool("ball")
+                if event.key == pygame.K_UP:
+                    self.spawn_ball_size += 1
+                    if self.spawn_ball_size > 20:
+                        self.spawn_ball_size = 20
+                if event.key == pygame.K_DOWN:
+                    self.spawn_ball_size -= 1
+                    if self.spawn_ball_size < 1:
+                        self.spawn_ball_size = 1
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.spawn_time_marker = pygame.time.get_ticks()
+                    self.game_object_manager.add(
+                        Ball(
+                            self.spawn_ball_size,
+                            event.pos,
+                            color.WHITE,
+                            self.spawn_ball_size * 2 * 3.14,
+                            (random.uniform(-100, 100), random.uniform(-100, 100)),
+                        )
+                    )
+        if pygame.mouse.get_pressed(3)[0]:
+            if self.spawn_time_marker + 50 > pygame.time.get_ticks():
+                return
+            self.spawn_time_marker = pygame.time.get_ticks()
+            mouse_position = pygame.mouse.get_pos()
+            self.game_object_manager.add(
+                Ball(
+                    self.spawn_ball_size,
+                    mouse_position,
+                    color.WHITE,
+                    self.spawn_ball_size * 2 * 3.14,
+                    (random.uniform(-100, 100), random.uniform(-100, 100)),
+                )
+            )
 
     def stop(self) -> None:
         self.running = False
