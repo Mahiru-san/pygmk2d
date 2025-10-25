e067fc94f21361b8f984482aede1c5bc669ce773
SteveNotJosh183 <minhmoc2709@gmail.com>
2025-10-25 13:45:12 +0700

    Refactor Renderable classes to use RenderParams and introduce RenderSpace enum for improved rendering context management

--- render/renderable.py
+++ render/renderable.py
@@ -1,15 +1,36 @@
-from typing import Callable
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Callable, Optional
+import weakref
 
+from ecs.component import Component
+from ecs.entity_manager import EntityManager
+from render.camera import Camera
 from render.context import RenderContext
+from render.transform import Transform
 
 
-class RenderableWrapper:
+class RenderSpace(Enum):
+    SCREEN = auto()
+    WORLD = auto()
+
+
+@dataclass(frozen=True)
+class RenderParams:
+    context: RenderContext
+    alpha: float
+    transform: Optional[Transform] = None
+    camera: Optional[Camera] = None
+
+
+class RenderableBase(Component):
     def __init__(
         self,
         render_function: Callable[
-            [RenderContext, float],
+            [RenderParams],
             None,
         ],
+        space: RenderSpace,
         layer: int = 0,
         depth: int = 0,
         visible: bool = True,
@@ -17,7 +38,60 @@ class RenderableWrapper:
         self.render_function = render_function
         self.layer = layer
         self.depth = depth
+        self.space = space
         self.visible = visible
 
-    def render(self, context: RenderContext, alpha: float) -> None:
-        self.render_function(context, alpha)
+    def render(self, params: RenderParams) -> None:
+        self.render_function(params)
+
+
+class UIRenderable(RenderableBase):
+
+    def __init__(
+        self,
+        render_function: Callable[
+            [RenderParams],
+            None,
+        ],
+        layer: int = 0,
+        depth: int = 0,
+        visible: bool = True,
+    ) -> None:
+        super().__init__(
+            render_function,
+            RenderSpace.SCREEN,
+            layer,
+            depth,
+            visible,
+        )
+
+
+class WorldRenderable(RenderableBase):
+
+    def __init__(
+        self,
+        render_function: Callable[
+            [RenderParams],
+            None,
+        ],
+        transform: Transform | None = None,
+        layer: int = 0,
+        depth: int = 0,
+        visible: bool = True,
+        debug_visible: bool = False,
+    ) -> None:
+        super().__init__(
+            render_function,
+            RenderSpace.WORLD,
+            layer,
+            depth,
+            visible,
+        )
+        self.transform_ref = weakref.ref(transform) if transform else None
+        self.debug_visible = debug_visible
+
+    def get_transform(self, em: EntityManager, entity: int) -> Transform | None:
+        transform = em.get_component(entity, Transform)
+        if transform:
+            return transform
+        return self.transform_ref
