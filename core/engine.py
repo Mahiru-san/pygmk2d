317cb7f825ba682e35751b129b08a1ba04f21e54
SteveNotJosh183 <minhmoc2709@gmail.com>
2025-10-26 23:24:39 +0700

    Refactor Engine class to replace input_manager with input_provider and rename set_min_fps to set_max_fps

--- core/engine.py
+++ core/engine.py
@@ -1,4 +1,5 @@
 from core.event_manager import EventManager
+from input.provider import InputProvider
 from render.context import RenderContext
 from ecs.entity_manager import EntityManager
 from ecs.system import System
@@ -13,14 +14,14 @@ class Engine:
         em: EntityManager,
         render_context: RenderContext,
         event_manager: EventManager,
-        input_manager: InputManager,
+        input_provider: InputProvider,
         clock: Clock,
         fixed_dt: float = 1 / 60,
     ) -> None:
         self.em = em
         self.render_system = RenderSystem(em, render_context)
         self.event_manager = event_manager
-        self.input_manager = input_manager
+        self.input_manager = InputManager(event_manager, input_provider)
         self.clock = clock
         self.fixed_dt = fixed_dt
         self._fixed_delta_systems: list[System] = []
@@ -28,7 +29,7 @@ class Engine:
         self.min_frame_time = 1 / 60  # Default to 60 FPS
         self.running = False
 
-    def set_min_fps(self, fps: int) -> None:
+    def set_max_fps(self, fps: int) -> None:
         self.min_frame_time = 1 / fps
 
     def set_fixed_dt(self, fixed_dt: float) -> None:
